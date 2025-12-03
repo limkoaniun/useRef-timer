80f563944113be9c4cfd418aa95d11ab39d1ff13
Guanyu Lin <limkoaniun@gmail.com>
2025-12-03 22:48:09 +1100

    add proj code

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {useRef, useState} from "react";
+
+const App = () => {
+    const timerRef = useRef(null);
+
+    const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+
+    const toggleTimer = () => {
+        if (isRunning) {
+            clearInterval(timerRef.current)
+            timerRef.current = null;
+        } else {
+            timerRef.current = setInterval(() => {
+                setTime((prevTime) => prevTime + 1)
+            }, 1000)
+        }
+
+        setIsRunning(!isRunning);
+    }
+    return (<div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
+        <h2 className="text-4xl font-semibold mt-4">
+            Timer: {time}
+        </h2>
+
+        <button
+            onClick={toggleTimer}
+            className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green600">{isRunning ? "Pause" : "Start"}
+        </button>
+    </div>)
+};
+
+export default App;
